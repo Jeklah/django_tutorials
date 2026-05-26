f7be3d023e8547aa2411b3e798e08c6d4ee09b74
Arthur Bowers <arthurbowers@gmail.com>
2026-05-26 09:29:31 +0100

    Adding some comments about __str__ being a python magic method.

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -7,5 +7,7 @@ class Member(models.Model):
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
 
+    # This method is used to return the string representation of the object
+    # This is a python magic method that is called when the object is printed
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
